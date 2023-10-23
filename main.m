641f8a8881b4285e7390ab9b0353b52503d3d93a
RavDesktop <ravindra.2000@hotmail.com>
2023-10-24 02:05:03 +1100

    TM5 DH Parameters + PLY Updated

--- main.m
+++ main.m
@@ -1,36 +1,24 @@
 
-function k = kbhit
-    % Check if a key has been pressed
-    k = false;
-    if ~isempty(get(get(0,'CurrentFigure'),'CurrentCharacter'))
-        k = true;
-    end
-end
 
-function key = getkey
-    % Get the key that has been pressed
-    key = get(get(0,'CurrentFigure'),'CurrentCharacter');
-    % Flush the keyboard buffer
-    while kbhit
-        get(get(0,'CurrentFigure'),'CurrentCharacter');
-    end
-end
+%Switch to create asynchronous stop -  when pressing Q, switch will change
+%to STOP and when pressed again will resume from stopped moment
 
 
 
 
+% switch ASTOP
+% 
+%     %
+%     case RUN
+% 
+% 
+%     case STOP
+%     w = waitforbuttonpress;
+% 
+%     ASTOP = 'RUN';
+% 
+% end
 
-stopFlag = false;
+TM5;
 
-while ~stopFlag
-    % Your robot control code goes here
-
-    % Check for user input asynchronously
-    if kbhit
-        key = getkey;
-        if key == 'q' % Check for a specific key (e.g., 'q') to stop
-            stopFlag = true;
-        end
-    end
-end
-
+pause;
