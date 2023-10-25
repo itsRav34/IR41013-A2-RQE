a49b8f052554f7d5844f017d4ec16f6a93414e44
EricKioko <ericnzioka11@gmail.com>
2023-10-25 18:50:31 +1100

    Env upodate
    Env

--- Environment/Environment.m
+++ Environment/Environment.m
@@ -3,13 +3,11 @@ close all
 clc
 hold on
 
-%Placement of floor, brickwalls, a light open curtain 
-% and an emergency stop button
+%Placement of floor, brickwalls & a light open curtain 
 PlaceObject('floor.ply',[0,-1,0]);
 PlaceObject('brickWall.ply',[3,2,0]);
 PlaceObject('brickWall2.ply',[-3,2,0]);
 PlaceObject('openCurtain.ply',[3,-1,0.8]);
-PlaceObject('emergencyStopButton.ply', [0.15,0.35,0.82]);
 
 %Placement of the two lab tables
 PlaceObject('labTable.ply',[0,0,0.41]);
@@ -19,12 +17,15 @@ PlaceObject('labTable.ply',[0,-2.5,0.41]);
 baseTR = transl([0.15,0,0.82])*trotz(-pi);
 r = LinearUR3(baseTR);
 r.model.animate(r.model.getpos());
+
+% Placement of fire extinguisher and an emergency stop button
+PlaceObject('fireExtinguisherElevated.ply', [1,1,0.45]);
 PlaceObject('emergencyStopButton.ply', [0.15,0.35,0.82]);
 
 %Placement of TM5-900 robot
-% baseTR = transl([-0.8,-2,0])*trotz(-pi);
-% s = TM5(baseTR);
-% s.model.animate(s.model.getpos());
+baseTR = transl([-0.8,-2,0])*trotz(-pi);
+s = TM5(baseTR);
+s.model.animate(s.model.getpos());
 
 %Placement of rack and 3 test tubes coloured RGB
 PlaceObject('testTubeR.ply', [-0.6,-2.4,0.85]);
@@ -37,5 +38,4 @@ PlaceObject('testTubeR.ply', [-0.15,-2.4,0.85]);
 PlaceObject('testTubeG.ply', [-0.15,-2.6,0.85]);
 PlaceObject('testTubeB.ply', [-0.15,-2.8,0.85]);
 PlaceObject('testTubeRack.ply', [-0.08,-1.99,0.6]);
-PlaceObject('fireExtinguisherElevated.ply', [1,1,0.45]);
 
